16d913dfdef7b942d24bb9f8e43f72d68a344378
Nikos Kechris <nkechris22@gmail.com>
2024-05-05 00:35:11 +0300

    Add favorites logic

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentCurrency =
     localStorage.getItem("currentCurrencyExtension") || "usd"; // Default currency
   let apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currentCurrency}&order=market_cap_desc`;
+  let allCoins = [];
 
   const fetchCryptoData = () => {
     const storedData = JSON.parse(
@@ -9,7 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
     );
     if (storedData && Date.now() - storedData?.timestamp < 5 * 60 * 1000) {
       // Use cached data if not expired
-      showCryptoData(storedData?.data);
+      allCoins = storedData.data;
+      showAllCoins();
     } else {
       // Fetch new data from API
       fetch(apiUrl)
@@ -20,11 +22,12 @@ document.addEventListener("DOMContentLoaded", function () {
           return response.json();
         })
         .then((data) => {
+          allCoins = data;
           localStorage.setItem(
             `cryptoData_${currentCurrency}`,
             JSON.stringify({ data: data, timestamp: Date.now() })
           );
-          showCryptoData(data);
+          showAllCoins();
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
@@ -33,105 +36,173 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 
-  const showCryptoData = (coins) => {
+  const showAllCoins = () => {
     const coinsContainer = document.getElementById("coins-container");
     coinsContainer.innerHTML = ""; // Clear previous content
 
-    if (coins?.length === 0) {
+    if (allCoins.length === 0) {
       displayNoData();
       return;
     }
 
-    coins?.forEach((coin) => {
-      const row = document.createElement("tr");
-
-      const imageAndNameCell = document.createElement("td");
-      const imageAndNameDiv = document.createElement("div");
-      imageAndNameDiv.classList.add("image-and-name");
-      imageAndNameCell.appendChild(imageAndNameDiv);
-
-      const image = document.createElement("img");
-      image.src = coin.image;
-      image.alt = coin.name;
-      image.classList.add("coin-image");
-
-      const name = document.createElement("span");
-      name.textContent = coin.name;
-
-      imageAndNameDiv.appendChild(image);
-      imageAndNameDiv.appendChild(name);
-
-      const priceCell = document.createElement("td");
-      let currencySymbol;
-
-      switch (currentCurrency) {
-        case "usd":
-          currencySymbol = "$";
-          break;
-        case "eur":
-          currencySymbol = "€";
-          break;
-        case "jpy":
-          currencySymbol = "¥";
-          break;
-        case "rub":
-          currencySymbol = "₽";
-          break;
-        case "cny":
-          currencySymbol = "¥";
-          break;
-        default:
-          currencySymbol = ""; // Handle any other cases if needed
-          break;
-      }
-
-      priceCell.textContent = `${currencySymbol}${new Intl.NumberFormat().format(
-        coin.current_price.toFixed(2)
-      )}`;
-      const priceChangeCell = document.createElement("td");
-      priceChangeCell.textContent =
-        coin.price_change_percentage_24h.toFixed(2) + "%";
-      priceChangeCell.classList.add("price-change");
-      if (coin.price_change_percentage_24h < 0) {
-        priceChangeCell.classList.add("negative");
-      }
-
-      row.appendChild(imageAndNameCell);
-      row.appendChild(priceCell);
-      row.appendChild(priceChangeCell);
-
+    allCoins.forEach((coin) => {
+      const row = createCoinRow(coin);
       coinsContainer.appendChild(row);
     });
+  };
+
+  const showFavoriteCoins = () => {
+    const favoriteCoins =
+      JSON.parse(localStorage.getItem("favoriteCoins")) || [];
+    const filteredCoins = allCoins.filter((coin) =>
+      favoriteCoins.includes(coin.id)
+    );
+    const coinsContainer = document.getElementById("coins-container");
+    coinsContainer.innerHTML = ""; // Clear previous content
+
+    if (filteredCoins.length === 0) {
+      displayNoData();
+      return;
+    }
+
+    filteredCoins.forEach((coin) => {
+      const row = createCoinRow(coin);
+      coinsContainer.appendChild(row);
+    });
+  };
+
+  const createCoinRow = (coin) => {
+    const row = document.createElement("tr");
+
+    const imageAndNameCell = document.createElement("td");
+    const imageAndNameDiv = document.createElement("div");
+    imageAndNameDiv.classList.add("image-and-name");
+    imageAndNameCell.appendChild(imageAndNameDiv);
+
+    const image = document.createElement("img");
+    image.src = coin.image;
+    image.alt = coin.name;
+    image.classList.add("coin-image");
+
+    const name = document.createElement("span");
+    name.textContent = coin.name;
+
+    imageAndNameDiv.appendChild(image);
+    imageAndNameDiv.appendChild(name);
+
+    const priceCell = document.createElement("td");
+    priceCell.textContent = `${getCurrencySymbol(
+      currentCurrency
+    )}${coin.current_price.toFixed(2)}`;
+
+    const priceChangeCell = document.createElement("td");
+    priceChangeCell.textContent = `${coin.price_change_percentage_24h.toFixed(
+      2
+    )}%`;
+    if (coin.price_change_percentage_24h < 0) {
+      priceChangeCell.classList.add("negative");
+    }
+
+    const favoriteCell = document.createElement("td");
+    const favoriteButton = document.createElement("div");
+    favoriteButton.classList.add("favorite-button");
+
+    // Check if the coin is a favorite
+    const favoriteCoins =
+      JSON.parse(localStorage.getItem("favoriteCoins")) || [];
+    const isFavorite = favoriteCoins.includes(coin.id);
+
+    // Create SVG heart icon
+    const heartSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${
+      isFavorite ? "red" : "none"
+    }" stroke="${
+      isFavorite ? "none" : "red"
+    }" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M20 4.5a5.36 5.36 0 0 0-7.55 0L12 5.94l-1.45-1.45a5.36 5.36 0 0 0-7.55 7.55L12 21l8.45-8.45a5.36 5.36 0 0 0 0-7.55z"></path>
+    </svg>
+  `;
+
+    favoriteButton.innerHTML = heartSVG;
+    favoriteButton.addEventListener("click", function () {
+      toggleFavorite(coin.id);
+      // Toggle heart color and border color after clicking
+      favoriteButton.innerHTML = isFavorite ? emptyHeartSVG : heartSVG;
+    });
+
+    favoriteCell.appendChild(favoriteButton);
+
+    row.appendChild(imageAndNameCell);
+    row.appendChild(priceCell);
+    row.appendChild(priceChangeCell);
+    row.appendChild(favoriteCell);
+
+    return row;
   };
 
   const displayNoData = () => {
     const coinsContainer = document.getElementById("coins-container");
     coinsContainer.innerHTML =
-      "<tr><td colspan='3'>No data available</td></tr>";
+      "<tr><td colspan='4'>No data available</td></tr>";
   };
 
   const displayError = () => {
     const coinsContainer = document.getElementById("coins-container");
     coinsContainer.innerHTML =
-      "<tr><td colspan='3'>Error fetching data</td></tr>";
+      "<tr><td colspan='4'>Error fetching data</td></tr>";
+  };
+
+  const toggleFavorite = (coinId) => {
+    let favoriteCoins = JSON.parse(localStorage.getItem("favoriteCoins")) || [];
+    const index = favoriteCoins.indexOf(coinId);
+    if (index === -1) {
+      favoriteCoins.push(coinId);
+    } else {
+      favoriteCoins.splice(index, 1);
+    }
+    localStorage.setItem("favoriteCoins", JSON.stringify(favoriteCoins));
+
+    // Update displayed coins based on the current active tab
+    const allCoinsTab = document.getElementById("all-coins-tab");
+    if (allCoinsTab.classList.contains("active-tab")) {
+      showAllCoins();
+    } else {
+      showFavoriteCoins();
+    }
+  };
+
+  const getCurrencySymbol = (currency) => {
+    switch (currency) {
+      case "usd":
+        return "$";
+      case "eur":
+        return "€";
+      case "jpy":
+        return "¥";
+      case "rub":
+        return "₽";
+      case "cny":
+        return "¥";
+      default:
+        return "";
+    }
   };
 
   fetchCryptoData();
 
-  const searchInput = document.getElementById("search-input");
-  searchInput.addEventListener("input", function () {
-    const searchText = this.value.toLowerCase();
-    const coinRows = document.querySelectorAll("#coins-container tr");
-    coinRows.forEach((row) => {
-      const coinName = row
-        .querySelector(".image-and-name span")
-        .textContent.toLowerCase();
-      if (coinName.includes(searchText)) {
-        row.style.display = "table-row";
-      } else {
-        row.style.display = "none";
-      }
-    });
+  const allCoinsTab = document.getElementById("all-coins-tab");
+  const favoriteCoinsTab = document.getElementById("favorite-coins-tab");
+
+  allCoinsTab.addEventListener("click", function () {
+    allCoinsTab.classList.add("active-tab");
+    favoriteCoinsTab.classList.remove("active-tab");
+    showAllCoins();
+  });
+
+  favoriteCoinsTab.addEventListener("click", function () {
+    favoriteCoinsTab.classList.add("active-tab");
+    allCoinsTab.classList.remove("active-tab");
+    showFavoriteCoins();
   });
 
   const changeCurrencySelect = document.getElementById(
@@ -143,23 +214,17 @@ document.addEventListener("DOMContentLoaded", function () {
     apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currentCurrency}&order=market_cap_desc`;
     localStorage.setItem("currentCurrencyExtension", currentCurrency);
     fetchCryptoData();
-
-    searchInput.value = "";
   });
 
-  //Dark mode logic
+  // Dark mode logic
+  const darkModeToggle = document.getElementById("dark-mode-toggle");
   const darkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
-  if (darkModeEnabled) {
-    document.body.classList.add("dark-mode");
-    document.getElementById("dark-mode-toggle").checked = true;
-  }
+  darkModeToggle.checked = darkModeEnabled;
+  document.body.classList.toggle("dark-mode", darkModeEnabled);
 
-  document
-    .getElementById("dark-mode-toggle")
-    .addEventListener("change", function () {
-      const darkModeEnabled =
-        document.getElementById("dark-mode-toggle").checked;
-      document.body.classList.toggle("dark-mode", darkModeEnabled);
-      localStorage.setItem("darkModeEnabled", darkModeEnabled);
-    });
+  darkModeToggle.addEventListener("change", function () {
+    const darkModeEnabled = this.checked;
+    document.body.classList.toggle("dark-mode", darkModeEnabled);
+    localStorage.setItem("darkModeEnabled", darkModeEnabled);
+  });
 });
